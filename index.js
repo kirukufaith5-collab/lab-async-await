1fd48eb30d1bd5abc4ae209184c6925b86756b67
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-04-16 01:30:11 +0300

    Fetch data from an API

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code here!
+function displayPosts(posts){
+    const postList =document.getElementById('post-list');
+}
+//loop through the posts list
+posts.forEach(post => {
+
+  //create a new <li> tag
+  const li = document.createElement('li');
+  //set the text content of the title of the post
+  li.textContent = post.title;
+  //create a new <p> tag and add the body
+  const p = document.createElement('p');
+    p.textContent = post.body;
+    //append the <p> tag to the <li> tag
+    li.appendChild(p);
+    //append the <li> tag to the <ul> tag
+    postList.appendChild(li);
+});
+
+//fetch the posts from the API
+async function fetchAndDisplayPosts() {
+  try {
+    console.log("Fetching posts...")
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    displayPosts(data);
+     
+
+    
+
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
+}
+fetchAndDisplayPosts();
