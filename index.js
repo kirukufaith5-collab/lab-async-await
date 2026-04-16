303d7f14e8af57f293e7a433c6cd5c3c799eae69
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-04-16 23:27:12 +0300

    Adjust the code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code here!
 function displayPosts(posts){
-    const postList =document.getElementById('post-list');
+    postList.innerHTML = '';
 }
 //loop through the posts list
 posts.forEach(post => {
@@ -8,11 +8,17 @@ posts.forEach(post => {
   //create a new <li> tag
   const li = document.createElement('li');
   //set the text content of the title of the post
-  li.textContent = post.title;
+  li.className = "post-card";
+
+  //create a new h1 tag and add the title
+  const h1 = document.createElement('h1');
+  h1.textContent = post.title;
+
   //create a new <p> tag and add the body
   const p = document.createElement('p');
     p.textContent = post.body;
-    //append the <p> tag to the <li> tag
+    //append the <p> and <h1> to the <li> tag
+    li.appendChild(h1);
     li.appendChild(p);
     //append the <li> tag to the <ul> tag
     postList.appendChild(li);
@@ -21,16 +27,20 @@ posts.forEach(post => {
 //fetch the posts from the API
 async function fetchAndDisplayPosts() {
   try {
+    //show loading ,hide list
+    loadingIndicator.style.display = 'block';
+    postList.style.display = 'none';
     console.log("Fetching posts...")
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await response.json();
     displayPosts(data);
-     
-
-    
 
   } catch (error) {
     console.error('Error fetching posts:', error);
   }
 }
-fetchAndDisplayPosts();
+ 
+
+//shuffle the posts to stimulate a "random" tab
+const shufflePosts=data.sort( => 0.5 - Math.random()).slice(0, 10);
+fetchAndDisplayPosts(shufflePosts);
